261877bf6614d7178fffe07e7f567290ac27a508
iharzh <iharzhuk22@gmail.com>
2022-02-19 08:51:06 +0100

    Setup sequelize connection

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { connectDb } from './src/infrastructure/db';
 
 const app = express();
 
 const PORT = 5000;
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port: ${PORT}`)
+    })
+});
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`)
-})
---
+++ server/src/infrastructure/db/index.ts
@@ -0,0 +1,18 @@
+import {Sequelize} from 'sequelize';
+
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  database: 'ddd',
+  username: 'postgres',
+  password: 'password'
+})
+
+export const connectDb = async() => {
+  try {
+    await sequelize.authenticate()
+  } catch(e) {
+    console.log(e)
+  }
+}
+
